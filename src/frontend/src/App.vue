53f6a1796f10add4b87e2a0cd66b8921efc36407
Volobuev <petr.volobuev@geropharm.com>
2021-11-18 13:32:21 +0300

    fix: clear old styles

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -14,44 +14,6 @@ export default {
 };
 </script>
 
-<style lang="scss">
-@import '~@/assets/scss/app';
-
-// #app {
-//   position: fixed;
-//   width: 100%;
-//   height: 100%;
-//   background-color: $blue-600;
-//   top: 0;
-//   left: 0;
-// }
-
-// .main {
-//   position: absolute;
-//   top: 50%;
-//   left: 50%;
-//   transform: translate(-50%, -50%);
-//   color: $white-900;
-
-//   img {
-//     display: block;
-//     margin: 0 auto;
-//   }
-
-//   h1 {
-//     @include m-s36-h21;
-//     text-align: center;
-//     margin-bottom: 0;
-//   }
-
-//   p {
-//     font-size: 20px;
-//     line-height: 30px;
-//     text-align: center;
-//   }
-
-//   b {
-//     font-size: 1.2em;
-//   }
-// }
+<style lang="scss" scoped>
+  @import '~@/assets/scss/app';
 </style>
